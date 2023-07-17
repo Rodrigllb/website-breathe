0db2874c7dde94e49afd422f4b5cb6ed0c809565
Rodrigo Liberato <rdrigoll7@gmail.com>
2023-07-16 21:27:55 -0300

    Modified style, add new style in project

--- assets/script/script-home-page.js
+++ assets/script/script-home-page.js
@@ -1,21 +1,22 @@
-const startButton = document.getElementById('img-button-start');
-const stopButton = document.getElementById('img-button-stop');
-const elementBreathe = document.getElementById('animation-element-circle');
-const textElement = document.getElementById('text-element');
+const startButton = document.getElementById('img-button-start');/*Botão de iniciar a animação */
+const stopButton = document.getElementById('img-button-stop');/*Botão de parar a animação. */
+const elementBreathe = document.getElementById('animation-element-circle');/*Elemento da animação. */
+const colorBody = document.body;
 
 function startButtonCLicking(){
-    textElement.style.display = 'none';/*Retiro o texto "Just breathe" */
+    colorBody.style.backgroundColor = '#003a69';/*Mudo para uma cor mais profunda. */
     elementBreathe.style.display ='flex';/*Exibo a animação*/
     startButton.style.display = "none";/*Escondo o botão de iniciar*/
     stopButton.style.display = "flex";/*Exibo o botão de parar */
+
 }
 function stopButtonClicking(){
-    textElement.style.display = 'flex';/*Exibo o texto "Just breathe" */
+    colorBody.style.backgroundColor = '#007de4'/*Exibo a cor inicial novamente. */
     elementBreathe.style.display ='none';/*Retiro a animação*/
     startButton.style.display = "flex"; /*Exibo novamente o botão de iniciar*/
     stopButton.style.display = "none";/*Escondo o botão de stop*/
 }
 
 
-startButton.addEventListener('click', startButtonCLicking);/*Quando clicar o botão em imagem ele chama a função especifica. */
-stopButton.addEventListener('click', stopButtonClicking);
+startButton.addEventListener('click', startButtonCLicking);/*Quando clicar o botão em imagem ele chama a função especifica.*/
+stopButton.addEventListener('click', stopButtonClicking);/*Quando clicar o botão de stop, chamar a função de pausa.*/
