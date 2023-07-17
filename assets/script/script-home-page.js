a8520308a5642cc0be2c95430640797a7d5f6928
Rodrigo Liberato <rdrigoll7@gmail.com>
2023-07-17 13:55:54 -0300

    update animation and add guides

--- assets/script/script-home-page.js
+++ assets/script/script-home-page.js
@@ -3,11 +3,13 @@ const stopButton = document.getElementById('img-button-stop');/*Botão de parar 
 const elementBreathe = document.getElementById('animation-element-circle');/*Elemento da animação. */
 const colorBody = document.body;
 
+
 function startButtonCLicking(){
-    colorBody.style.backgroundColor = '#003a69';/*Mudo para uma cor mais profunda. */
+    colorBody.style.backgroundColor = '#001527';/*Mudo para uma cor mais profunda. */
     elementBreathe.style.display ='flex';/*Exibo a animação*/
     startButton.style.display = "none";/*Escondo o botão de iniciar*/
     stopButton.style.display = "flex";/*Exibo o botão de parar */
+    var exesong = new Audio('../../Audio/brathe - Maconi Union - Weightless.mp3');
 
 }
 function stopButtonClicking(){
@@ -16,7 +18,6 @@ function stopButtonClicking(){
     startButton.style.display = "flex"; /*Exibo novamente o botão de iniciar*/
     stopButton.style.display = "none";/*Escondo o botão de stop*/
 }
-
 
 startButton.addEventListener('click', startButtonCLicking);/*Quando clicar o botão em imagem ele chama a função especifica.*/
 stopButton.addEventListener('click', stopButtonClicking);/*Quando clicar o botão de stop, chamar a função de pausa.*/
