6b612f1532b2747ebeec71c5d88f1fd4ffafb224
Rodrigo Liberato <rdrigoll7@gmail.com>
2023-07-25 21:58:18 -0300

    Update and add rain sound file

--- assets/script/script-home-page.js
+++ assets/script/script-home-page.js
@@ -1,7 +1,8 @@
 const startButton = document.getElementById('img-button-start');/*Botão de iniciar a animação */
 const stopButton = document.getElementById('img-button-stop');/*Botão de parar a animação. */
 const elementBreathe = document.getElementById('animation-element-circle');/*Elemento da animação. */
-
+const soundRainButton = document.getElementById('rainsound');/*Botão para iniciar um som de chuva ao fundo.*/
+const elementPanelAudio = document.getElementById('element-audio');
 
 function startButtonCLicking(){
     elementBreathe.style.display ='flex';/*Exibo a animação*/
@@ -14,6 +15,11 @@ function stopButtonClicking(){
     startButton.style.display = "flex"; /*Exibo novamente o botão de iniciar*/
     stopButton.style.display = "none";/*Escondo o botão de stop*/
 }
+function soundRainClicking(){
+    elementPanelAudio.style.display = 'flex';
+    
+}
 
 startButton.addEventListener('click', startButtonCLicking);/*Quando clicar o botão em imagem ele chama a função especifica.*/
 stopButton.addEventListener('click', stopButtonClicking);/*Quando clicar o botão de stop, chamar a função de pausa.*/
+soundRainButton.addEventListener('click', soundRainClicking);/*Quando clicado o botão iniciar um som de chuva ao fundo!*/
