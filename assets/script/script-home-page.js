c78a8b1c07edd86a6fbfc6f17dc346d64ee9efa7
Rodrigo Liberato <rdrigoll7@gmail.com>
2023-07-29 14:44:26 -0300

    Add breakpoints

--- assets/script/script-home-page.js
+++ assets/script/script-home-page.js
@@ -1,8 +1,14 @@
 const startButton = document.querySelector('.img-button-start');/*Botão de iniciar a animação */
+
 const stopButton = document.querySelector('.img-button-stop');/*Botão de parar a animação. */
+
 const elementBreathe = document.querySelector('.animation-element-circle');/*Elemento da animação. */
-const soundRainButton = document.querySelector('.rainsound');/*Botão para iniciar um som de chuva ao fundo.*/
-const elementPanelAudio = document.querySelector('.element-audio');
+
+const soundRainButton = document.querySelector('.StartRainSound');/*Botão para iniciar um som de chuva ao fundo.*/
+
+const stopRainSong = document.querySelector('.stopRainSound');/*Botão de parar som de chuva.*/
+
+const elementPanelAudio = document.querySelector('.element-audio');/*Painel de audio.*/
 
 function startButtonCLicking(){
     elementBreathe.style.display ='flex';/*Exibo a animação*/
@@ -17,9 +23,16 @@ function stopButtonClicking(){
 }
 function soundRainClicking(){
     elementPanelAudio.style.display = 'flex';/*Mostro o player de musica.*/
-    
+    soundRainButton.style.display = 'none';
+    stopRainSong.style.display = 'flex';
+}
+function stopRainClicking(){
+    elementPanelAudio.style.display = 'none';/*Mostro o player de musica.*/
+    soundRainButton.style.display = 'flex';
+    stopRainSong.style.display = 'none';
 }
 
 startButton.addEventListener('click', startButtonCLicking);/*Quando clicar o botão em imagem ele chama a função especifica.*/
 stopButton.addEventListener('click', stopButtonClicking);/*Quando clicar o botão de stop, chamar a função de pausa.*/
 soundRainButton.addEventListener('click', soundRainClicking);/*Quando clicado o botão iniciar um som de chuva ao fundo!*/
+stopRainSong.addEventListener('click', stopRainClicking);
